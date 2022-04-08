72877d17a71ee96b8653c9a57294d6e5114fad6f
Simone Ferrari <simo.ferrari135@gmail.com>
2022-04-08 19:24:50 +0200

    aggiungo dettaglio piatto

--- progetto/app/src/main/java/com/example/pwbaseprova/DettaglioPiattoActivity.java
+++ progetto/app/src/main/java/com/example/pwbaseprova/DettaglioPiattoActivity.java
@@ -2,7 +2,13 @@ package com.example.pwbaseprova;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.pwbaseprova.piatti.Piatto;
+import com.squareup.picasso.Picasso;
 
 public class DettaglioPiattoActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class DettaglioPiattoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dettaglio_piatto);
+
+        Intent intent = getIntent();
+        Piatto piatto = intent.getParcelableExtra("item-value");
+
+        TextView title = findViewById(R.id.titlePiatto);
+        TextView description = findViewById(R.id.descriptionPiatto);
+        ImageView image = findViewById(R.id.imagePiatto);
+
+        title.setText(piatto.getName());
+        description.setText(piatto.getDescription());
+        Picasso.get().load(piatto.getImage()).into(image);
     }
 }
